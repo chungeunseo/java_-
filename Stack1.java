13a678ef7ade0b57c208adc46684fc27d4ec0b88
chungeunseo <wjddmstj123@naver.com>
2022-01-24 21:14:09 +0900

    bj 10828 stack1

---
+++ Stack1.java
@@ -0,0 +1,85 @@
+package com.baekjoon;
+import java.util.Scanner;
+public class Stack1 {
+	public static int[] stack;
+	public static int size = 0;
+	
+	public static void push(int n) {
+		stack[size] = n;
+		size++;
+	}
+  
+	public static int pop() {
+		if(size == 0) {
+			return -1;
+		}
+		else {
+			int pop = stack[size - 1];
+			stack[size - 1] = 0;
+			size--;
+			return pop;
+		}
+	}
+    
+	public static int size() {
+		return size;
+	}
+    
+	public static int empty() {
+		if(size == 0) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+    
+	public static int top() {
+		if(size == 0) {
+			return -1;
+		}
+		else {
+			return stack[size - 1];
+		}
+	}
+ 
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		int N = in.nextInt();
+ 
+		stack = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+        
+			String str = in.next();
+ 
+			switch (str) {            
+			case "push":
+				push(in.nextInt());
+				break;
+                
+			case "pop":
+				sb.append(pop()).append('\n');
+				break;
+                
+			case "size":
+				sb.append(size()).append('\n');
+				break;
+                
+			case "empty":
+				sb.append(empty()).append('\n');
+				break;
+                
+			case "top":
+				sb.append(top()).append('\n');
+				break;
+			}
+ 
+		}
+		System.out.println(sb);
+	}
+ 
+
+}
