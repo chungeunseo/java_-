924a651fe3602e610d5c01a8f7964a35c4e9e2ca
chungeunseo <wjddmstj123@naver.com>
2022-02-09 16:44:04 +0900

    백준 11279 최대힙

---
+++ algorithm/bj/Main_BJ_11279_최대힙.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_BJ_11279_최대힙 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine()); 
+	}
+}
